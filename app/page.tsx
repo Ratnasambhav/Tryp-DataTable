377db037fd9cb1ae90e0792caaef442ae1f5e60b
Ratansambhav Priyadarshi <rpriyadarshi@purestorage.com>
2023-07-15 17:57:57 +0530

    Initial table implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 'use client'
 
-import { Album, AlbumResponse } from '@/types/album'
+
+import Image from 'next/image'
+import { Badge, Link, Tag } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { useState, useEffect } from 'react'
+
+import DataTable from '@/components/DataTable'
+
+import type { Album, AlbumResponse } from '@/types/album'
 
 const API_ENDPOINT = 'https://itunes.apple.com/search?term=michael+jackson&entity=album'
 
@@ -27,10 +34,74 @@ export default function Home() {
     }
   }
 
+  const getHeaders = () => [
+    'Artwork',
+    'Name',
+    'Genre',
+    'Release Date',
+    'Track Count',
+    '',
+  ]
+
+  const getRows = () => albums.map((album) => [
+    {
+      value: renderArtwork(album.artworkUrl100, album.collectionName),
+      isNumeric: false,
+    },
+    {
+      value: album.collectionName,
+      isNumeric: false,
+    },
+    {
+      value: renderGenre(album.primaryGenreName),
+      isNumeric: false,
+    },
+    {
+      value: renderReleaseDate(album.releaseDate),
+      isNumeric: false,
+    },
+    {
+      value: renderTrackCount(album.trackCount),
+      isNumeric: false,
+    },
+    {
+      value: renderLink(album.collectionViewUrl),
+      isNumeric: false,
+    },
+  ])
+
+  const renderArtwork = (artworkUrl: string, alt: string) => {
+    const imageStyle = {
+      borderRadius: '5px',
+      boxShadow: '20px 20px 60px #BEBEBE, -20px -20px 60px #FFFFFF',
+    }
+    return (
+      <Image src={artworkUrl} alt={alt} width={72} height={72} style={imageStyle} />
+    )
+  }
+
+  const renderReleaseDate = (releaseDate: string) => {
+    const date = new Date(releaseDate)
+    return (
+      <span>{date.toLocaleDateString()}</span>
+    )
+  }
+
+  const renderTrackCount = (trackCount: number) => (
+    <Badge variant='subtle' borderRadius='full'>{trackCount}</Badge>
+  )
+
+  const renderGenre = (genre: string) => (
+    <Tag variant='solid' borderRadius='full'>{genre}</Tag>
+  )
+
+  const renderLink = (url: string) => (
+    <Link href={url} isExternal>Listen <ExternalLinkIcon mb='5px' /></Link>
+  )
+
   return (
     <main>
-      <h1>Tryp Data Table</h1>
-      <pre>{JSON.stringify(albums, null, 2)}</pre>
+      <DataTable headers={getHeaders()} rows={getRows()} caption="Michael Jackson's Albums" />
     </main>
   )
 }
--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -1,1 +1,53 @@
+import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, TableContainer } from '@chakra-ui/react'
 
+export type DataTableCell = {
+  value: React.ReactNode,
+  isNumeric?: boolean,
+}
+
+export type DataTableProps = {
+  headers: string[]
+  rows: DataTableCell[][]
+  caption: React.ReactNode
+  sortable?: boolean
+  paginated?: boolean
+}
+
+export default function DataTable({
+  rows,
+  headers,
+  caption,
+  sortable = false,
+  paginated = false,
+}: DataTableProps) {
+  const renderTableBody = () => {
+    if (!rows?.length) {
+      return <span>No Data</span>
+    }
+
+    return rows.map((row) => (
+      <Tr>
+        {row.map(({ value, isNumeric}) => (
+            <Td isNumeric={isNumeric}>{value}</Td>
+          )
+        )}
+      </Tr>
+    ))
+  }
+
+  return (
+    <TableContainer>
+      <Table variant='simple'>
+        <TableCaption placement='top'>{caption}</TableCaption>
+        <Thead>
+          <Tr>
+            {headers.map((header) => <Th key={header}>{header}</Th>)}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {renderTableBody()}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  )
+}
