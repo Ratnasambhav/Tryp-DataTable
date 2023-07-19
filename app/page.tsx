e2619e87624fe25f9a28241372cfc2c033fc9aeb
Ratansambhav Priyadarshi <rpriyadarshi@purestorage.com>
2023-07-19 12:52:40 +0530

    Added filtering, pagination to table

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,68 @@
 'use client'
 
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import { Badge, Button, Box, Flex, Text } from '@chakra-ui/react'
+import Head from 'next/head';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import {
+  Badge,
+  Button,
+  Box,
+  Center,
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  Skeleton,
+  Stack,
+  Switch,
+  Text,
+} from '@chakra-ui/react';
 
-import { formatDate } from '@/utils'
-import DataTable, { DataTableCell } from '@/components/DataTable'
+import DataTable, { DataTableCell } from '../components/DataTable';
+import useDebounce from '../hooks/useDebounce';
+import { formatDate } from '../utils';
 
-import type { Album, AlbumResponse } from '@/types/album'
+import type { NextPage } from 'next';
+import type { Album, AlbumResponse } from '@/types/album';
 
-const API_ENDPOINT = 'https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=michael+jackson&entity=album'
+const API_ENDPOINT = 'https://itunes.apple.com/search';
+const DEFAULT_ARTIST = 'Björk';
+const ALBUM_ENTITY_NAME = 'album';
+const DEFAULT_ALBUMS_PER_PAGE = 10;
 
-export default function Home() {
-  const [error, setError] = useState<any>(null)
-  const [albums, setAlbums] = useState<Album[]>([])
-  const [isLoading, setLoading] = useState<boolean>(false)
+const Home: NextPage = () => {
+  const [error, setError] = useState<any>(null);
+  const [albums, setAlbums] = useState<Album[]>([]);
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [isPaginationEnabled, setIsPaginationEnabled] = useState<boolean>(true);
+  const [albumsPerPage, setAlbumsPerPage] = useState<number>(
+    DEFAULT_ALBUMS_PER_PAGE
+  );
+  const [artist, setArtist] = useState<string>(DEFAULT_ARTIST);
+  const [tableCaption, setTableCaption] = useState<string>('');
+  const [search, setSearch] = useState<string>('');
+  const searchArtist = useDebounce<string>(artist, 500);
 
   useEffect(() => {
-    fetchUsers()
-  }, [])
+    searchArtist && fetchAlbums();
+  }, [searchArtist]);
 
-  const fetchUsers = async () => {
-    setLoading(true)
+  const fetchAlbums = async () => {
+    setLoading(true);
     try {
-      const res = await fetch(API_ENDPOINT, { headers: { 'Access-Control-Allow-Origin':'*' }})
-      const { results } = await res.json() as unknown as AlbumResponse
-      setAlbums(results)
+      const url = new URL(API_ENDPOINT);
+      url.searchParams.append('term', searchArtist.toLowerCase());
+      url.searchParams.append('entity', ALBUM_ENTITY_NAME);
+      const res = await fetch(url.toString());
+      const { results, resultCount } = (await res.json()) as AlbumResponse;
+      setAlbums(results);
+      setTableCaption(`${artist}'s Albums (${resultCount} albums)`);
     } catch (err) {
-      setError(err)
+      setError(err);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   const getHeaders = () => [
     'Album',
@@ -39,7 +70,7 @@ export default function Home() {
     'Release Date',
     'Track Count',
     '',
-  ]
+  ];
 
   const getColumnsConfig = () => [
     { sortable: true },
@@ -47,87 +78,178 @@ export default function Home() {
     { sortable: true },
     { sortable: true },
     { sortable: false },
-  ]
+  ];
 
-  const getRows = (): DataTableCell[][] => albums.map((album) => [
-    {
-      value: album.collectionName,
-      content: renderAlbumName(album.collectionName, album.artworkUrl100),
-    },
-    {
-      value: album.primaryGenreName,
-      content: renderGenre(album.primaryGenreName),
-    },
-    {
-      value: new Date(album.releaseDate),
-      content: renderReleaseDate(album.releaseDate),
-    },
-    {
-      value: album.trackCount,
-      content: renderTrackCount(album.trackCount),
-    },
-    {
-      value: null,
-      content: renderAction(album.collectionViewUrl),
-    },
-  ])
+  const getRows = (): DataTableCell[][] =>
+    albums.map((album) => [
+      {
+        value: album.collectionName,
+        content: renderAlbumName(album.collectionName, album.artworkUrl100),
+      },
+      {
+        value: album.primaryGenreName,
+        content: renderGenre(album.primaryGenreName),
+      },
+      {
+        value: new Date(album.releaseDate),
+        content: renderReleaseDate(album.releaseDate),
+      },
+      {
+        value: album.trackCount,
+        content: renderTrackCount(album.trackCount),
+      },
+      {
+        value: null,
+        content: renderAction(album.collectionViewUrl),
+      },
+    ]);
 
   const renderReleaseDate = (releaseDate: string) => {
-    return (
-      <Text>{formatDate(releaseDate)}</Text>
-    )
-  }
+    return <Text>{formatDate(releaseDate)}</Text>;
+  };
 
   const renderTrackCount = (trackCount: number) => (
     <Badge
-      fontSize='sm'
-      variant='subtle'
-      borderRadius='full'
-      colorScheme='orange'
+      fontSize="sm"
+      variant="subtle"
+      borderRadius="full"
+      colorScheme="orange"
     >
       {trackCount}
     </Badge>
-  )
+  );
 
   const renderGenre = (genre: string) => (
-    <Text fontSize='md' noOfLines={1}>{genre}</Text>
-  )
+    <Text fontSize="md" noOfLines={1}>
+      {genre}
+    </Text>
+  );
 
   const renderAction = (url: string) => (
     <Button
-      size='sm'
-      colorScheme='orange'
+      size="sm"
+      colorScheme="orange"
       onClick={() => window.open(url, '_blank')?.focus()}
     >
       Listen
     </Button>
-  )
+  );
 
   const renderAlbumName = (name: string, artworkUrl: string) => {
     const imageStyle = {
       borderRadius: '5px',
       boxShadow: '20px 20px 60px #BEBEBE, -20px -20px 60px #FFFFFF',
+    };
+    return (
+      <Flex alignItems="center" gap="2">
+        <Image
+          alt={name}
+          width={32}
+          height={32}
+          src={artworkUrl}
+          style={imageStyle}
+        />
+        <Text fontSize="lg" noOfLines={1}>
+          {name}
+        </Text>
+      </Flex>
+    );
+  };
+
+  const renderForm = () => (
+    <Flex gap="4" my="16px" direction="row" wrap="wrap">
+      <InputGroup maxW="320px">
+        <InputLeftAddon children="Artist:" />
+        <Input
+          type="text"
+          value={artist}
+          onChange={(e) => setArtist(e.target.value)}
+        />
+      </InputGroup>
+      <InputGroup maxW="256px">
+        <InputLeftAddon children="Albums Per Page:" />
+        <Input
+          type="number"
+          value={albumsPerPage}
+          isDisabled={!isPaginationEnabled}
+          onChange={(e) =>
+            setAlbumsPerPage(Math.max(e.target.valueAsNumber, 1))
+          }
+        />
+      </InputGroup>
+      <Flex align="center" gap="2">
+        <Text fontWeight="semibold">Pagination</Text>
+        <Switch
+          isChecked={isPaginationEnabled}
+          onChange={() => setIsPaginationEnabled((enabled) => !enabled)}
+        />
+      </Flex>
+      <Flex>
+        <InputGroup maxW="320px">
+          <InputLeftAddon children="Search:" />
+          <Input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </InputGroup>
+      </Flex>
+    </Flex>
+  );
+
+  const renderTable = () => {
+    if (isLoading) {
+      return (
+        <Stack>
+          <Skeleton height="56px" />
+          <Skeleton height="56px" />
+          <Skeleton height="56px" />
+          <Skeleton height="56px" />
+          <Skeleton height="56px" />
+        </Stack>
+      );
+    }
+    if (error) {
+      return (
+        <Center>
+          <Text color="red" fontWeight="bold">
+            Sorry, something went wrong :(
+          </Text>
+        </Center>
+      );
     }
     return (
-      <Flex alignItems='center' gap='2'>
-        <Image src={artworkUrl} alt={name} width={32} height={32} style={imageStyle} />
-        <Text fontSize='lg' noOfLines={1}>{name}</Text>
-      </Flex>
-    )
-  }
+      <DataTable
+        sortable
+        search={search}
+        rows={getRows()}
+        caption={tableCaption}
+        headers={getHeaders()}
+        perPage={albumsPerPage}
+        paginated={isPaginationEnabled}
+        columnsConfig={getColumnsConfig()}
+      />
+    );
+  };
 
   return (
-    <Box m='16px'>
-      <Text fontSize='3xl' fontWeight='black'>Tryp Data Table</Text>
-      <Box my='24px'>
-        <DataTable
-          sortable
-          rows={getRows()}
-          headers={getHeaders()}
-          columnsConfig={getColumnsConfig()}
-          caption={'Michael Jackson\'s Albums'}
-        />
-      </Box>
-    </Box>
-  )
-}
+    <>
+      <Head>
+        <title>DataTable</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Box m="16px">
+          <Text fontSize="3xl" fontWeight="black">
+            DataTable
+          </Text>
+          <Box>{renderForm()}</Box>
+          <Box my="24px">{renderTable()}</Box>
+        </Box>
+      </main>
+    </>
+  );
+};
+
+export default Home;
--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -1,147 +1,227 @@
 'use client'
+import { useEffect, useMemo, useState } from 'react';
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
+import {
+  Button,
+  Center,
+  Flex,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react';
 
-import { useEffect, useState, useMemo } from 'react'
-import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
-import { Button, Center, Flex, Text, Table, Thead, Tbody, Tr, Th, Td, TableCaption, TableContainer } from '@chakra-ui/react'
-
-import { compareObjects } from '@/utils';
+import Pagination from './Pagination';
+import { compareObjects } from '../utils';
 
 export type DataTableCell = {
-  value: any
-  content: React.ReactNode
-}
+  value: any;
+  content: React.ReactNode;
+};
 
-export type ColumnConfig = {
-  sortable: boolean
-}
+type ColumnConfig = {
+  sortable: boolean;
+};
 
 export type DataTableProps = {
-  headers: string[]
-  rows: DataTableCell[][]
-  caption: React.ReactNode
-  maxWidth?: string
-  columnsConfig?: ColumnConfig[]
-  sortable?: boolean
-  paginated?: boolean
-}
+  search?: string;
+  perPage?: number;
+  headers: string[];
+  sortable?: boolean;
+  paginated?: boolean;
+  rows: DataTableCell[][];
+  caption: React.ReactNode;
+  columnsConfig?: ColumnConfig[];
+};
 
-enum SortDirection { ASC, DSC, NONE }
+enum SortDirection {
+  ASC,
+  DSC,
+  NONE,
+}
 
 export default function DataTable({
   rows,
   headers,
   caption,
-  maxWidth,
+  search = '',
+  perPage = 10,
   sortable = false,
   paginated = false,
   columnsConfig = [],
 }: DataTableProps) {
-  const [sortIndex, setSortIndex] = useState<number | null>(null)
-  const [sortDirection, setSortDirection] = useState<SortDirection>(SortDirection.NONE)
-  const [sortedRows, setSortedRows] = useState<DataTableCell[][]>(rows)
-
-  useEffect(() => setSortedRows(rows), [rows])
+  const [page, setPage] = useState<number>(1);
+  const [sortIndex, setSortIndex] = useState<number | null>(null);
+  const [sortDirection, setSortDirection] = useState<SortDirection>(
+    SortDirection.NONE
+  );
+  const [sortedRows, setSortedRows] = useState<DataTableCell[][]>(rows);
 
   useEffect(() => {
-    if (!sortIndex) {
-      return
-    }
-    const nextSortedRows = sortRows(sortIndex)
-    setSortedRows(() => nextSortedRows)
-  }, [sortDirection, sortIndex])
+    const filteredRows = search.length
+      ? rows.filter(([{ value }]) =>
+          (value as string).toLowerCase().includes(search.toLowerCase())
+        )
+      : rows;
+
+    const nextSortedRows =
+      sortIndex === null || sortDirection === SortDirection.NONE
+        ? filteredRows
+        : filteredRows.toSorted((rowA, rowB) => {
+            const valueA = rowA.at(sortIndex)?.value;
+            const valueB = rowB.at(sortIndex)?.value;
+            const modifier = sortDirection === SortDirection.ASC ? 1 : -1;
+
+            switch (typeof valueA) {
+              case 'number':
+                return (valueA - valueB) * modifier;
+              case 'string':
+                return valueA.localeCompare(valueB) * modifier;
+              default:
+                return (
+                  (valueA instanceof Date && valueB instanceof Date
+                    ? valueA > valueB
+                      ? 1
+                      : -1
+                    : compareObjects(valueA, valueB)) * modifier
+                );
+            }
+          });
+
+    setSortedRows(nextSortedRows);
+  }, [rows, search, sortDirection, sortIndex]);
 
   const handleSort = (index: number) => {
     if (sortIndex !== index) {
-      setSortIndex(() => index)
-      setSortDirection(SortDirection.ASC)
+      setSortIndex(index);
+      setSortDirection(SortDirection.ASC);
     } else {
-      setSortDirection((dir) => dir === 2 ? 0 : dir + 1);
+      setSortDirection((dir) => (dir === SortDirection.NONE ? 0 : dir + 1));
     }
-  }
-
-  const sortRows = (index: number): DataTableCell[][] => {
-    console.log("==> SORTING", { index, sortDirection })
-    return rows.toSorted((rowA, rowB) => {
-      const valueA = rowA.at(index)?.value
-      const valueB = rowB.at(index)?.value
-      const modifier = sortDirection === SortDirection.ASC ? 1 : -1;
-    
-      switch (typeof valueA) {
-        case 'number':
-          return (valueA - valueB) * modifier
-        case 'string':
-          return valueA.localeCompare(valueB) * modifier
-        default:
-          return (
-            (valueA instanceof Date && valueB instanceof Date)
-              ? (valueA > valueB ? 1 : -1)
-              : compareObjects(valueA, valueB)
-          ) * modifier
-      }
-    })
-  }
+  };
 
   const tableHeaders = useMemo(() => (
     <Thead>
       <Tr>
         {headers.map((header, index) => (
-          <Th key={header} px='8px'>
-            {
-              (!sortable || !columnsConfig.at(index)?.sortable)
-                ? (
-                  <Text fontSize='md' fontWeight='bold' color='orange.400'>{header}</Text>
-                ) : (
-                  <Button onClick={() => handleSort(index)} variant='ghost' size='xs' padding={0} _hover={{ backgroundColor: 'inherit' }}>
-                    <Flex gap='2' alignItems='center'>
-                      <Text fontSize='md' fontWeight='bold'>{header}</Text>
-                      <Flex direction='column'>
-                        <ChevronUpIcon
-                          boxSize={4}
-                          color='orange.900'
-                          opacity={index === sortIndex && sortDirection === SortDirection.ASC ? 1 : 0.2}
-                        />
-                        <ChevronDownIcon
-                          boxSize={4}
-                          color='orange.900'
-                          opacity={index === sortIndex && sortDirection === SortDirection.DSC ? 1 : 0.2}
-                        />
-                      </Flex>
-                    </Flex>
-                  </Button>
-                )
-            }
+          <Th key={header} px="8px">
+            {!sortable || !columnsConfig.at(index)?.sortable ? (
+              <Text fontSize="md" fontWeight="bold" color="orange.400">
+                {header}
+              </Text>
+            ) : (
+              <Button
+                onClick={() => handleSort(index)}
+                variant="ghost"
+                size="xs"
+                padding={0}
+                _hover={{ backgroundColor: 'inherit' }}
+              >
+                <Flex gap="2" alignItems="center">
+                  <Text fontSize="md" fontWeight="bold">
+                    {header}
+                  </Text>
+                  <Flex direction="column">
+                    <ChevronUpIcon
+                      boxSize={4}
+                      color="orange.900"
+                      opacity={
+                        index === sortIndex &&
+                        sortDirection === SortDirection.ASC
+                          ? 1
+                          : 0.2
+                      }
+                    />
+                    <ChevronDownIcon
+                      boxSize={4}
+                      color="orange.900"
+                      opacity={
+                        index === sortIndex &&
+                        sortDirection === SortDirection.DSC
+                          ? 1
+                          : 0.2
+                      }
+                    />
+                  </Flex>
+                </Flex>
+              </Button>
+            )}
           </Th>
         ))}
       </Tr>
     </Thead>
-  ), [columnsConfig, headers, sortIndex, sortDirection])
+  ), [headers, columnsConfig, sortDirection, sortIndex]);
+
+  const renderBody = () => {
+    const start = paginated ? Math.max((page - 1) * perPage, 0) : 0;
+    const end = paginated ? page * perPage : -1;
+    return (
+      <Tbody>
+        {sortedRows.slice(start, end).map((row, index) => (
+          <Tr
+            _hover={{ backgroundColor: 'orange.100' }}
+            key={`${headers[index]}-${index}`}
+          >
+            {row.map(({ content, value }) => (
+              <Td isNumeric={typeof value === 'number'} padding={3} key={value}>
+                {content}
+              </Td>
+            ))}
+          </Tr>
+        ))}
+      </Tbody>
+    );
+  };
+
+  const renderPagination = () => {
+    if (!paginated || perPage >= sortedRows.length) {
+      return null;
+    }
+
+    const maxPage = Math.ceil(sortedRows.length / perPage);
+    const onNext = () =>
+      setPage((currentPage) => Math.min(currentPage + 1, maxPage));
+    const onPrev = () => setPage((currentPage) => Math.max(currentPage - 1, 1));
+    const onClick = (index: number) => setPage(index);
+
+    return (
+      <Center>
+        <Pagination
+          current={page}
+          max={maxPage}
+          onNext={onNext}
+          onPrev={onPrev}
+          onClick={onClick}
+        />
+      </Center>
+    );
+  };
 
   if (!sortedRows.length) {
     return (
       <Center>
-        <Text fontWeight='bold' fontSize='lg'>No Data</Text>
+        <Text fontWeight="bold" fontSize="lg">
+          No Data
+        </Text>
       </Center>
-    )
+    );
   }
 
   return (
     <>
       <TableContainer>
-        <Table variant='unstyled' colorScheme='orange' maxWidth={maxWidth}>
-          <TableCaption color='orange.500'>{caption}</TableCaption>
+        <Table variant="unstyled" colorScheme="orange">
+          <TableCaption color="orange.500">{caption}</TableCaption>
           {tableHeaders}
-          <Tbody>
-            {sortedRows.map((row, index) => (
-              <Tr _hover={{ backgroundColor: 'orange.100'  }} key={`${headers[index]}-${index}`}>
-                {row.map(({ content, value }) => (
-                    <Td isNumeric={typeof value === 'number'} padding={3} key={value}>{content}</Td>
-                  )
-                )}
-              </Tr>
-            ))}
-          </Tbody>
+          {renderBody()}
         </Table>
       </TableContainer>
+      {renderPagination()}
     </>
-  )
+  );
 }
